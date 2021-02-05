f2ea165616fa8f5827ff914fb7db2cbeb7d3098e
Seth Cohen <smcohen82@gmail.com>
2021-02-05 15:42:07 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -3,8 +3,10 @@ require 'pry'
 
 def snake_it_up(string)
   if string[0] == "s"
+    10.times do
+      "s" + string
+    end
     binding.pry
-    10.times { "s" + string }
   end
   binding.pry
   string
